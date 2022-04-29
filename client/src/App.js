c625f934d9c05571b5dcd2534222df2cc9a5b19e
fedeprat <97050765+fedeprat@users.noreply.github.com>
2022-04-29 16:16:19 -0300

    added client

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Chat from './components/Chat/Chat'
+import Join from './components/Join/Join'
+
+export default function App() {
+    return <Router>
+        <Route path="/" exact component={Join} />
+        <Route path="/chat" component={Chat} />
+    </Router>
+}
